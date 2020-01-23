02b47345482e0c51125799767fb0c7356c0999d3
mcapuccini <marco.capuccini@it.uu.se>
2020-01-23 16:10:56 +0000

    run a test

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import gym
 
-env_to_wrap = gym.make("LunarLander-v2")
-env = gym.wrappers.Monitor(env_to_wrap, "/host-home/Desktop/test", force=True)
-frame = env_to_wrap.reset()
+env_to_wrap = gym.make("CartPole-v0")
+env = gym.wrappers.Monitor(env_to_wrap, "test", force=True)
+frame = env.reset()
 is_done = False
 while not is_done:
-  action = env_to_wrap.action_space.sample()
+  action = env.action_space.sample()
   _, _, is_done, _ = env.step(action)
 env.close()
 env_to_wrap.close()
