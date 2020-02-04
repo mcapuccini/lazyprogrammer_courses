dc704560ef8aa663376d7db6ed461041ff340597
mcapuccini <marco.capuccini@it.uu.se>
2020-02-04 15:15:34 +0000

    fix q bins

--- q_learning_bins.py
+++ q_learning_bins.py
@@ -1,22 +1,27 @@
-# %% Imports and defs
 import numpy as np
 import gym
 import matplotlib.pyplot as plt
 
+N = 10000
 CART_POSITION_BINS = np.linspace(-2.4, 2.4, 9)
 CART_VELOCITY_BINS = np.linspace(-2, 2, 9) 
 POLE_ANGLE_BINS = np.linspace(-0.4, 0.4, 9)
 POLE_VELOCITY_BINS = np.linspace(-3.5, 3.5, 9)
 
+def build_state(features):
+    return int("".join(map(lambda feature: str(int(feature)), features)))
+
+def to_bin(value, bins):
+    return np.digitize(x=[value], bins=bins)[0]
+
 def obs2state(obs):
     cart_pos, cart_vel, pole_angle, pole_vel = obs
-    features = [
-        np.digitize(x=[cart_pos], bins=CART_POSITION_BINS)[0],
-        np.digitize(x=[cart_vel], bins=CART_VELOCITY_BINS)[0],
-        np.digitize(x=[pole_angle], bins=POLE_ANGLE_BINS)[0],
-        np.digitize(x=[pole_vel], bins=POLE_VELOCITY_BINS)[0]
-    ]
-    return int("".join(map(lambda feature: str(int(feature)), features)))
+    return build_state([
+        to_bin(cart_pos, CART_POSITION_BINS),
+        to_bin(cart_vel, CART_VELOCITY_BINS),
+        to_bin(pole_angle, POLE_ANGLE_BINS),
+        to_bin(pole_vel, POLE_VELOCITY_BINS),
+    ])
 
 def plot_running_avg(totalrewards):
     N = len(totalrewards)
@@ -27,40 +32,45 @@ def plot_running_avg(totalrewards):
     plt.title("Running Average")
     plt.show()
 
+def eps_greedy(env, Q_state):
+    if np.random.random() < eps:
+        return env.action_space.sample()
+    else:
+        return np.argmax(Q_state)
+
 def play_episode(env, Q, eps, gamma=0.9, alpha=1e-2):
     done = False
     obs = env.reset()
     state = obs2state(obs)
     tot_reward = 0
-    iters = 0
     while not done:
-        action = np.argmax(Q[state])
-        if np.random.random() < eps:
-            action = env.action_space.sample()
+        # Step
+        action = eps_greedy(env, Q[state])
         obs_prime, rew, done, _ = env.step(action)
-        state_prime = obs2state(obs_prime)
-        if done and iters < 199: # if game over before step 200 it's bad
-            rew = -300
-        Q[state, action] += alpha * (rew + gamma * np.max(Q[state_prime]) - Q[state, action])
-        state = state_prime
-        iters += 1
+        # Update tot rew
         tot_reward += rew
-    return Q, tot_reward, iters
+        # Update Q
+        if done and tot_reward < 199: # if game over before step 200 it's bad
+            rew = -300
+        state_prime = obs2state(obs_prime)
+        Q[state, action] += alpha * (rew + gamma * np.max(Q[state_prime]) - Q[state, action])
+        # Update state
+        state = state_prime
+    return Q, tot_reward
 
-# %%
 # Init
 env = gym.make('CartPole-v0')
-N = 10000
 tot_rewards = np.empty(N)
 num_states = 10**env.observation_space.shape[0]
 num_actions = env.action_space.n
 Q = np.random.uniform(low=-1, high=1, size=(num_states, num_actions))
+
 # Main loop
 for n in range(N):
     eps = 1.0/np.sqrt(n+1)
-    Q, tot_reward, iters = play_episode(env, Q, eps)
+    Q, tot_reward = play_episode(env, Q, eps)
     tot_rewards[n] = tot_reward
     if n % 100 == 0:
-        print("episode:", n, "total reward:", tot_reward, "eps:", eps)
+        print("episode:", n, ", tot rew run avg:", tot_rewards[max(0, n-100):(n+1)].mean(), ", eps:", eps)
 # Plot
 plot_running_avg(tot_rewards)
